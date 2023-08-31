0ca06af827a05a00cd8bee546693d456f3084793
Andreas <Formarn3@gmail.com>
2023-08-31 15:13:33 +0200

    added a test for website sprint 0

---
+++ test/GOWEBAPI/main.go
@@ -0,0 +1,25 @@
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func main() {
+	request1()
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func aboutMe(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About Me!")
+	fmt.Println("Endpoint Hit: aboutMe")
+}
+
+func request1() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/aboutMe", aboutMe)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
