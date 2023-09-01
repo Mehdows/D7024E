c9a1644373f8033c02f816fdfc43b22b84667e7f
Mehdows <marcus99.ma@gmail.com>
2023-09-01 10:20:56 +0200

    test

--- sprint0/GOWEBAPI/main.go
+++ sprint0/GOWEBAPI/main.go
@@ -1,40 +1,48 @@
 package main
 
 import (
-	
+	"html/template"
+	"net/http"
 )
 
-
-var SERVER_PORT string 
+var SERVER_PORT string
 var CLIENT_ADDRESS string // possibly = "http://localhost:8081"
 
 var packets_recieved int
 var packets_sent int
 
 type Response struct {
-    someString string
+	someString string
 }
 
-var response Response;
+var response Response
 
 var recieved int
 var sent int
-
 
 func setResponse() {
 	response = Response{"Success!"}
 }
 
+/*
 func sendPacket(port int) {
 	packets_sent++
 	c := newClient(CLIENT_ADDRESS)
 	setResponse()
 
-	res := exchangeJson(c, response)
-	
+	//res := exchangeJson(c, response)
+
+}*/
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	var filename = "index.html"
+	t, err := template.ParseFiles(filename)
+	if err != nil {
+		panic(err)
+	}
+	t.ExecuteTemplate(w, filename, nil)
 }
 
-
 func main() {
-	
+
 }
