bf0c3280c40dcb4718fa1d242cdfb0b24c1292a1
Mehdows <marcus99.ma@gmail.com>
2023-09-01 08:48:27 +0200

    changed to a different server client code

--- sprint0/GOWEBAPI/main.go
+++ sprint0/GOWEBAPI/main.go
@@ -1,66 +1,39 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
+	
 )
+
+
+var SERVER_PORT string 
+var CLIENT_ADDRESS string // possibly = "http://localhost:8081"
+
+var packets_recieved int
+var packets_sent int
 
 type Response struct {
     someString string
 }
 
+var response Response;
+
 var recieved int
 var sent int
 
 
+func setResponse() {
+	response = Response{"Success!"}
+}
 
 func sendPacket(port int) {
-	struc := &Response{"Sent Success"}
-	jsonStr, err := json.Marshal(struc)
-	if err != nil {
-		fmt.Println("Error")
-	}
-	resp, err := http.Post(fmt.Sprint("localhost:", port), "application/json", bytes.NewBuffer(jsonStr))
-	if err != nil {
-		fmt.Println("Error")
-	}
-	defer resp.Body.Close()
-	strjson, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error")
-	}
-	res := ""
-	json.Unmarshal(strjson, &res )
+	packets_sent++
+	c := newClient(CLIENT_ADDRESS)
+	setResponse()
 
-
-}
-func listen(rw http.ResponseWriter, req *http.Request) {
-	body, err := io.ReadAll(req.Body)
-	if err != nil {
-		panic(err)
-	}
-	json.Unmarshal(body, &recieved)
-
-
-	respond(rw, "Recieved Success")
+	res := exchangeJson(c, response)
+	
 }
 
-func respond(rw http.ResponseWriter, str string) {
-
-	fmt.Println("Sending response...")
-
-	rw.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(rw).Encode(str)
-}
-
-func SetupServer(port string) {
-	funcHandle := "/sendPacket"
-	http.HandleFunc(funcHandle, listen)
-	http.ListenAndServe(port, nil)
-}
 
 func main() {
 	
