9ef12c17f83680c5c2f20c560ad6257fe0bfd2a9
Andreas <Formarn3@gmail.com>
2023-09-25 09:46:33 +0200

    Finished with CLI

--- kademlia/cli_handler.go
+++ kademlia/cli_handler.go
@@ -8,7 +8,7 @@ import (
 )
 
 
-func Cli_handler() {
+func Cli_handler(kademlia *Kademlia) {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Enter Text: ")
@@ -20,17 +20,17 @@ func Cli_handler() {
 		res := strings.Split(text, " ")
 		choice := res[0]
 
+		// If the user wants to get a file write its hash
 		if choice == "get" && len(res) == 2 {
 			fmt.Println("I will get a file with hash: ", res[1])
-			
-			
+			kademlia.LookupData(res[1])
 
+		// If the user wants to store a file write its value
 		} else if choice == "put" {
 			fmt.Println("I will store a file with value: ", res[1:])
 			newres := strings.Join(res[1:], " ")
 			bytearr := []byte(newres)
-			fmt.Println(bytearr)
-			//Store(bytearr)
+			kademlia.Store(bytearr)
 
 		} else if choice == "exit" {
 			fmt.Println("Exiting...")
