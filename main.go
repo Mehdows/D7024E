33a2df72b687bd66c02abe86919983b446ef10d4
Andreas <Formarn3@gmail.com>
2023-10-09 13:06:43 +0200

    Finished with a working main (is executable)

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Mehdows/D7024E/kademlia"
 )
 
-func main () {
-	fmt.Println("Hello world!")
-	Kademlia := NewKademliaNode("localhost:8080")
+func main() {
+	Kademlia := kademlia.NewKademliaNode("localhost:8080")
 	kademlia.Cli_handler(&Kademlia)
 }
