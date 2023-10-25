9bc71fdeb11bb7015e9a3e0cc5f3a2b41187ecf3
Andreas <Formarn3@gmail.com>
2023-10-25 10:16:36 +0200

    test test more

--- kademlia/kademlia.go
+++ kademlia/kademlia.go
@@ -70,7 +70,9 @@ func (kademlia *Kademlia) LookupContact(target *KademliaID) (closestNode *Contac
 		
 		shortList = append(shortList, response.Data.(*responseFindNodeData).Contacts...)
 		closest = shortList[len(shortList)-1]
-		
+
+
+		fmt.Print("Cloest: ", closest.ID.String(), " OldClose: ", oldClose.ID.String(), "\n")
 		if closest == oldClose {
 			break
 		} else {
@@ -105,8 +107,9 @@ func (kademlia *Kademlia) handleLookupData(message Message, conn net.Conn) {
 func (kademlia *Kademlia) Store(data []byte) {
 	sha1 := sha1.Sum(data)
 	key := hex.EncodeToString(sha1[:])
-
+	fmt.Println("1")
 	location := NewKademliaID(key)
+	fmt.Println("Look up contact", location.String())
 	recipient := kademlia.LookupContact(location)
 	fmt.Println("STOOOOOOOOOOOORE")
 	go kademlia.network.SendStoreMessage(*recipient, location, []byte(data))
